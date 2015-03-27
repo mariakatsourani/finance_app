e841e8cd545d516deb9feb7434aab32dee2e34b2
mariakatsourani <vilyalscpt@hotmail.com>
2015-03-27 14:07:13 +0100

    stock class

--- controllers/stock.php
+++ controllers/stock.php
@@ -2,15 +2,46 @@
 class Stock extends Controller{
 
     public function index(){
-
+        $db = Database::getInstance();
+        $sql = "SELECT * FROM stocks";
+        $data = $db->query_sql($sql, []);
+        $this->view('all_stocks', $data);
     }
 
-    public function show($stock){
-        $data = ['name' => $stock ];
+    public function show($symbol){
+        $db = Database::getInstance();
+        $sql = "SELECT * FROM stocks WHERE symbol=:symbol";
+        $params = ['symbol' => $symbol];
+        $stock = $db->query_sql($sql, $params);
+        //var_dump($stock[0]);
+        $data = $stock[0];
         $this->view('stock_summary', $data);
     }
 
-    public function update(){
+    public function insert()
+    {
+        $db = Database::getInstance();
+
+        //get data
+        $url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22AAPL%22%2C%20%22GOOG%22%2C%20%22MSFT%22%2C%20%22AMZN%22%2C%20%22FB%22%2C%20%22EBAY%22%2C%20%22HM-B.ST%22%2C%20%22TWTR%22%2C%20%22SNE%22%2C%20%22VOLV-B.ST%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=';
+        $content = file_get_contents($url);
+        $info = json_decode($content, true);
+        $info = $info['query']['results']['quote'];
+        var_dump($info);
+
+        /*foreach($info as $stock){
+            $data = array(
+                "name" => $stock['Name'],
+                "symbol" => $stock['Symbol'],
+                "last_price" => $stock['Ask'],
+                "last_change_procent" => $stock['PercentChange'],
+                "last_change" => $stock['Change'],
+                "available_for_shop" => $stock['AverageDailyVolume'],
+            );
+            $db->insert('stocks', $data);
+        }
+        */
 
     }
+
 }
